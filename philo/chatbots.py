aee0c4b383ae8cc8f787994caf968aaa7c7bafe6
JJ <jj.stankowicz@gmail.com>
2024-02-27 00:58:01 -0500

    Add chatbot and tests.

---
+++ philo/chatbots.py
@@ -0,0 +1,60 @@
+from typing import Generator
+from openai import OpenAI
+from philo.utils import MessageType
+
+
+class OpenAIChat:
+    def __init__(
+        self,
+        model: str = "gpt-4-1106-preview",
+        role_str: str = "You are a helpful assistant.",
+    ) -> None:
+        self.client = OpenAI()
+        self.role_str = role_str
+        if model not in ["gpt-4-1106-preview", "gpt-4", "gpt-3.5-turbo"]:
+            raise ValueError(f"Model {model} not available for chat.")
+        self.model = model
+        self.current_answer = ""
+        self.current_question = ""
+        self.reset_messages()
+
+    def reset_messages(self) -> None:
+        self.messages = [{"role": "system", "content": self.role_str}]
+
+    def add_message(self, role: str, content: str) -> None:
+        self.messages.append({"role": role, "content": content})
+
+    def get_response(self) -> str:
+        # For every message in the chat history, keep only the role and content
+        messages = [
+            {"role": message["role"], "content": message["content"]} for message in self.messages
+        ]
+        response = self.client.chat.completions.create(model=self.model, messages=messages)
+        return response
+
+    def stream_response(self) -> Generator[str, None, None]:
+        messages = [
+            {"role": message["role"], "content": message["content"]} for message in self.messages
+        ]
+        for response in self.client.chat.completions.create(
+            model=self.model,
+            messages=messages,
+            stream=True,
+        ):
+            partial_response = response.choices[0].delta.content or ""
+            yield partial_response
+
+    def set_messages_from(self, messages: MessageType) -> None:
+        self.messages = messages
+
+    def get_messages(self) -> MessageType:
+        return self.messages
+
+    def send_receive(self, user_question: str) -> str:
+        self.current_question = user_question
+        self.add_message("user", user_question)
+        response = self.get_response()
+        response = response.choices[0].message.content
+        self.add_message("assistant", response)
+        self.current_answer = response
+        return response
---
+++ tests/test_chatbots.py
@@ -0,0 +1,24 @@
+import unittest
+from philo.chatbots import OpenAIChat
+
+
+class TestOpenAIChat(unittest.TestCase):
+    def setUp(self):
+        self.chatbot = OpenAIChat()
+        return super().__init__()
+
+    def test_send_receive(self):
+        self.chatbot.send_receive("Hello")
+        # Check that the first message is from the system
+        self.assertEqual(self.chatbot.messages[0]["role"], "system")
+        original_first_message = self.chatbot.messages[0]
+        # Check that the second message is from the user and is "Hello"
+        self.assertEqual(self.chatbot.messages[1]["role"], "user")
+        self.assertEqual(self.chatbot.messages[1]["content"], "Hello")
+        # Check that a message was added with role "assistant" and "content" is a string
+        self.assertIsInstance(self.chatbot.messages[-1]["content"], str)
+        self.assertEqual(self.chatbot.messages[-1]["role"], "assistant")
+        # Check that the chatbot can be reset
+        self.chatbot.reset_messages()
+        # Check that the messages have been reset
+        self.assertEqual(self.chatbot.messages, [original_first_message])
