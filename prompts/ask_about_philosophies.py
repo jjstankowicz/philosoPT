84e771f503bbb3f18969581c7a44020e1ceb8108
JJ <jj.stankowicz@gmail.com>
2024-02-27 01:34:15 -0500

    Create prompt.

---
+++ prompts/ask_about_philosophies.py
@@ -0,0 +1,21 @@
+version = {}
+
+version[-1] = """
+List three popular phiolosophies and a simple, single sentence description of each.
+Return the results as a list of python dictionaries of the form:
+[
+    {'name': philosophy_name, 'description': philosophy_description},
+    ...
+]
+Do not include any other text in the output.
+"""
+
+version[0] = """
+List many popular philosophies and a simple, single sentence description of each.
+Return the results as a list of python dictionaries of the form:
+[
+    {'name': philosophy_name, 'description': philosophy_description},
+    ...
+]
+Do not include any other text in the output.
+"""
