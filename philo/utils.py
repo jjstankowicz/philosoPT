594108bf7fdd99a43ace256ded9436ecd609ede1
JJ <jj.stankowicz@gmail.com>
2024-02-28 01:41:01 -0500

    Add test to list of dicts method.

--- philo/utils.py
+++ philo/utils.py
@@ -3,10 +3,12 @@ from typing import List, Dict
 
 MessageType = List[Dict[str, str]]
 
+
 # A method to get parent directory of the current file
 def get_repo_root() -> str:
-    out = os.path.abspath(os.path.join(__file__,os.pardir,os.pardir))
+    out = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir))
     return out
+
 
 # A method to get the prompt from a file
 def get_prompt(prompt_name: str, version_number: int) -> str:
@@ -16,3 +18,21 @@ def get_prompt(prompt_name: str, version_number: int) -> str:
         exec(f.read(), globals())
     # Return the version
     return version[version_number]
+
+
+import re
+from ast import literal_eval
+
+
+# Convert a string of text to a list of dictionaries
+def text_to_list_of_dicts(text: str) -> List[Dict[str, str]]:
+    pattern = r"(\w)'(\w)"
+    replacement = r"\1APOSTROPHEAPOSTROPHE\2"
+    # Remove newlines
+    out = text.replace("\n", "")
+    # Replace single quotes with escaped single quotes
+    out = re.sub(pattern, replacement, out)
+    # Replace 'APOTROPHEAPOSTROPHE' with escaped single quotes
+    out = out.replace("APOSTROPHEAPOSTROPHE", "\\'")
+    out = literal_eval(out)
+    return out
