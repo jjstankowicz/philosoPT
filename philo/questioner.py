808de3071d6f21b560786d7d5a97d6abc734392d
JJ <jj.stankowicz@gmail.com>
2024-02-28 01:40:39 -0500

    Start actions from philosophies.

--- philo/questioner.py
+++ philo/questioner.py
@@ -1,36 +1,58 @@
 import json
 import os
-from philo.utils import get_prompt
-from philo.chatbots import OpenAI
+from philo.utils import get_prompt, get_repo_root
+from philo.chatbots import OpenAIChat
 
 
 class Questioner:
+
     def __init__(self, fresh_start: bool = False):
-        self.chatbot = OpenAI()
+        self.history_file_path = os.path.join(get_repo_root(), "history.json")
+        self.chatbot = OpenAIChat()
         self.fresh_start = fresh_start
         self.read_history()
 
     def read_history(self):
         try:
-            with open("history.json", "r") as f:
+            if self.fresh_start:
+                # Delete the history file
+                os.remove(self.history_file_path)
+            with open(self.history_file_path, "r") as f:
                 self.history = json.load(f)
         except FileNotFoundError:
             # Create the file if it doesn't exist
-            if not os.path.exists("history.json"):
-                with open("history.json", "w") as f:
+            if not os.path.exists(self.history_file_path):
+                with open(self.history_file_path, "w") as f:
                     f.write("{}")
             self.history = {}
 
     def write_history(self):
-        with open("history.json", "w") as f:
+        with open(self.history_file_path, "w") as f:
             json.dump(self.history, f)
 
     def get_philosophies(self, version_number: int):
-        prompt = get_prompt("ask_about_philosophies", version_number=version_number)
+        prompt = get_prompt("ask_about_philosophies",
+                            version_number=version_number)
         if prompt in self.history:
             return self.history[prompt]
         else:
             out = self.chatbot.send_receive(prompt)
             self.history[prompt] = out
-            self.sync_history()
+            self.write_history()
+            return out
+
+    def get_actions_from_philosophies(
+        self,
+        version_number: int,
+        philosophy_dict: dict,
+    ):
+        prompt = get_prompt("ask_about_action_from_philosophy",
+                            version_number=version_number)
+        prompt.replace("{{ USER_INPUT }}", str(philosophy_dict))
+        if prompt in self.history:
+            return self.history[prompt]
+        else:
+            out = self.chatbot.send_receive(prompt)
+            self.history[prompt] = out
+            self.write_history()
             return out
