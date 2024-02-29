6bc67e2d1de1082a5213cad8bb459091b9c49dc9
JJ <jj.stankowicz@gmail.com>
2024-02-29 00:59:40 -0500

    Allow for scratchpad.

--- philo/utils.py
+++ philo/utils.py
@@ -16,13 +16,21 @@ from ast import literal_eval
 
 # Convert a string of text to a list of dictionaries
 def text_to_list_of_dicts(text: str) -> List[Dict[str, str]]:
+    out = text
+    if "##" in text:
+        # Identify the last line that starts with "#"
+        last_line = text.split("\n")
+        last_line = [line for line in last_line if line.startswith("#")]
+        last_line = last_line[-1]
+        out = out.split(last_line)[-1]
     pattern = r"(\w)'(\w)"
     replacement = r"\1APOSTROPHEAPOSTROPHE\2"
     # Remove newlines
-    out = text.replace("\n", "")
+    out = out.replace("\n", "")
     # Replace single quotes with escaped single quotes
     out = re.sub(pattern, replacement, out)
     # Replace 'APOTROPHEAPOSTROPHE' with escaped single quotes
     out = out.replace("APOSTROPHEAPOSTROPHE", "\\'")
+    out = out.replace("```", "")
     out = literal_eval(out)
     return out
