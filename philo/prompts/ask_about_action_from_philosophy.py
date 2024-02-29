6db9f3a9775e84f9ac16064a38cdc8188f393ebe
JJ <jj.stankowicz@gmail.com>
2024-02-29 00:12:05 -0500

    Delete old prompt.

--- philo/prompts/ask_about_action_from_philosophy.py
+++
@@ -1,8 +0,0 @@
-version[0] = """
-
-"""
-version[0] = version[0].replace("{{ FORMAT_STRING }}", FORMAT_STRING)
-version[0] = version[0].replace("{{ EXAMPLE_STRING }}", EXAMPLE_STRING)
-version[0] = version[0].replace("{{ USER_STRING }}", USER_STRING)
-
-version[-1] = version[0]
