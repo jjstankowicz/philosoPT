da7bc15b1bc607959342d6e0d596a984ac9313c7
JJ <jj.stankowicz@gmail.com>
2024-03-02 14:02:27 -0500

    Change printouts.

--- scripts/run_all.py
+++ scripts/run_all.py
@@ -6,12 +6,15 @@ VERBOSE = False
 
 def main():
     q = Questioner()
+    ### Get all philosophies ###
+    ###
+    print("Getting all philosophies...")
     philosophies = q.get_philosophies(prompt_version_number=1)
+    ### Get all actions from all philosophies ###
+    ###
+    print("Getting actions from all philosophies...")
     collect_actions_from_phiolosophies = []
     for philosophy_dict in philosophies:
-        if VERBOSE:
-            print(f'---{philosophy_dict["name"]}---')
-            print(philosophy_dict["description"])
         actions_from_philosophies = q.get_actions_from_philosophies(
             prompt_version_number=0,
             philosophy_dict=philosophy_dict,
@@ -22,32 +25,26 @@ def main():
                 "actions": actions_from_philosophies,
             }
         )
+    ### Collect actions into a list ###
+    ###
+    print("Collecting actions into a list...")
     collect_all_actions = []
     for action in collect_actions_from_phiolosophies:
-        if VERBOSE:
-            print(f'---{action["philo"]["name"]}---')
-            print(action["philo"]["description"])
-            print("")
         for action_dict in action["actions"]:
-            if VERBOSE:
-                print(action_dict["morality"])
-                print(action_dict["action"])
-                print(action_dict["reason"])
-                print("")
             collect_all_actions.append(action_dict["action"])
     if VERBOSE:
         for a in collect_all_actions:
             print(a)
+    ### Cluster the actions
+    ###
+    # print("Clustering actions...")
     action_clusters = q.get_action_clusters(
         prompt_version_number=0,
         action_list=collect_all_actions,
-        force_refresh=True,
+        verbose=True,
+        pbar=True,
+        # force_refresh=True,
     )
-    for cluster, actions in action_clusters.items():
-        print(f"---{cluster}---")
-        for action in actions:
-            print(" ", action)
-        print("")
     import pdb
 
     pdb.set_trace()
