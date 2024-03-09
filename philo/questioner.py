8996e32f156c5fe033c4df8c90d630164a867732
JJ <jj.stankowicz@gmail.com>
2024-03-09 00:11:57 -0500

    Document all questioner methods.

--- philo/questioner.py
+++ philo/questioner.py
@@ -22,12 +22,31 @@ class Questioner:
         self.read_history()
 
     def set_chatbot(self, model: str = "gpt-4-1106-preview") -> None:
+        """Set the chatbot to use for the questioner.
+
+        Args:
+            model (str, optional): The model name. Defaults to "gpt-4-1106-preview".
+        """
         self.chatbot = OpenAIChat(model=model)
 
     def log(self, text: str) -> None:
+        """Log a message.
+
+        Args:
+            text (str): Message to log.
+        """
         print(text)
 
     def log_chatbot(self, text: str, type: str = "") -> None:
+        """Pre-formated log for chatbot interactions.
+
+        Args:
+            text (str): The text to log.
+            type (str, optional): The type ["","prompt","response]. Defaults to "".
+
+        Raises:
+            ValueError: Invalid type.
+        """
         if type not in ["", "prompt", "response"]:
             raise ValueError(f"Invalid type: {type}")
         self.log(f"===== {type} =====")
@@ -36,7 +55,8 @@ class Questioner:
         self.log("=                =")
         self.log(f"=== End {type} ===")
 
-    def read_history(self):
+    def read_history(self) -> None:
+        """Read the history file."""
         try:
             if self.fresh_start:
                 # Delete the history file
@@ -50,7 +70,8 @@ class Questioner:
                     f.write("{}")
             self.history = {}
 
-    def write_history(self):
+    def write_history(self) -> None:
+        """Write the history file."""
         with open(self.history_file_path, "w") as f:
             json.dump(self.history, f, indent=2)
 
@@ -60,7 +81,21 @@ class Questioner:
         history_key: str,
         force_refresh: bool = False,
         max_retries: int = 15,
-    ):
+    ) -> str:
+        """Wrapper to send and receive messages from the chatbot.
+
+        Args:
+            prompt (str): The prompt to send to the chatbot.
+            history_key (str): The key to use for the history.
+            force_refresh (bool, optional): If true, rerun even if history_key is in history. Defaults to False.
+            max_retries (int, optional): Maximum number of retries. Defaults to 15.
+
+        Raises:
+            Exception: Failed after max_retries attempts.
+
+        Returns:
+            str: The response from the chatbot.
+        """
         retries = 0
         while retries < max_retries:
             seed = retries + 1
@@ -84,6 +119,7 @@ class Questioner:
                 # Handle exceptions (both from sending/receiving and parsing)
                 self.log("An error occurred; attempting retry.")
                 self.log(f"T={temperature}, seed={seed}")
+                self.log(f"Error: {e}")
                 retries += 1
                 if history_key in self.history:
                     time.sleep(5)  # Give the history time to update
@@ -100,10 +136,30 @@ class Questioner:
         # After exhausting max_retries, handle the failure case
         raise Exception(f"Failed after {max_retries} attempts.")
 
-    def get_key(self, prompt_name: str, prompt_version_number: int, *args):
+    def get_key(self, prompt_name: str, prompt_version_number: int, *args) -> str:
+        """Automatically generate a history key.
+
+        Args:
+            prompt_name (str): Name of the prompt.
+            prompt_version_number (int): Version number of the prompt.
+            *args: Additional arguments to include in the key.
+
+        Returns:
+            str: The history key, formatted as "prompt_name||prompt_version_number||args[0]||args[1]||..."
+        """
         return f"{prompt_name}||{prompt_version_number}||{'||'.join(args)}"
 
-    def set_philosophies(self, prompt_version_number: int, force_refresh: bool = False):
+    def set_philosophies(
+        self,
+        prompt_version_number: int,
+        force_refresh: bool = False,
+    ) -> None:
+        """Set the philosophies by sending a prompt to the chatbot.
+
+        Args:
+            prompt_version_number (int): The version number of the prompt.
+            force_refresh (bool, optional): If true, rerun even if history_key is in history. Defaults to False.
+        """
         prompt_name = "philosophies"
         pc = PromptConstructor(prompt_name=prompt_name, prompt_version_number=prompt_version_number)
         prompt = pc.get_prompt()
@@ -111,34 +167,68 @@ class Questioner:
         out = self.send_receive(prompt, history_key, force_refresh)
         self.philosophies = parse_structured_output(out)
 
-    def get_philosophies(self, prompt_version_number: int, force_refresh: bool = False):
-        self.set_philosophies(prompt_version_number, force_refresh)
-        return self.philosophies
-
-    def get_actions_from_philosophies(
+    def set_actions_from_philosophies(
         self,
         philosophy_dict: dict,
         prompt_version_number: int,
         force_refresh: bool = False,
-    ):
+    ) -> None:
+        """Determines the actions from a philosophy.
+
+        Args:
+            philosophy_dict (dict): A dictionary representing a philosophy.
+            prompt_version_number (int): The version number of the prompt.
+            force_refresh (bool, optional): If true, rerun even if history_key is in history. Defaults to False.
+        """
         prompt_name = "action_from_philosophy"
         pc = PromptConstructor(prompt_name=prompt_name, prompt_version_number=prompt_version_number)
         prompt = pc.get_prompt(user_input=str(philosophy_dict))
         history_key = self.get_key(prompt_name, pc.prompt_version_number, philosophy_dict["name"])
         out = self.send_receive(prompt, history_key, force_refresh)
-        return parse_structured_output(out)
+        self.actions_from_philosophies = parse_structured_output(out)
 
-    def get_clusters_to_actions(
+    def set_all_actions_from_philosophies(self) -> None:
+        """Set all actions from all philosophies."""
+        collect_actions_from_philosophies = []
+        for philosophy_dict in self.philosophies:
+            self.set_actions_from_philosophies(
+                prompt_version_number=0,
+                philosophy_dict=philosophy_dict,
+            )
+            collect_actions_from_philosophies.append(
+                {
+                    "philo": philosophy_dict,
+                    "actions": self.actions_from_philosophies,
+                }
+            )
+        self.all_actions_from_philosophies = collect_actions_from_philosophies
+
+    def set_all_actions(self) -> None:
+        """Collect all actions into a list."""
+        collect_all_actions = []
+        for action in self.all_actions_from_philosophies:
+            for action_dict in action["actions"]:
+                collect_all_actions.append(action_dict["action"])
+        self.all_actions = collect_all_actions
+
+    def set_clusters_to_actions(
         self,
         prompt_version_number: int,
-        action_list: List[str],
         force_refresh: bool = False,
         pbar: bool = False,
         verbose: bool = False,
-    ) -> Dict[str, List[Dict[str, str]]]:
+    ) -> None:
+        """Create a dictionary of clusters to actions.
+
+        Args:
+            prompt_version_number (int): The version number of the prompt.
+            force_refresh (bool, optional): If true, rerun even if history_key is in history. Defaults to False.
+            pbar (bool, optional): If true, show a progress bar. Defaults to False.
+            verbose (bool, optional): If true, log the prompt and response. Defaults to False.
+        """
         prompt_name = "determine_clusters"
         pc = PromptConstructor(prompt_name=prompt_name, prompt_version_number=prompt_version_number)
-        prompt = pc.get_prompt(user_input="\n".join(action_list))
+        prompt = pc.get_prompt(user_input="\n".join(self.all_actions))
         if verbose:
             self.log_chatbot(prompt, "prompt")
         history_key = self.get_key(prompt_name, pc.prompt_version_number)
@@ -152,9 +242,9 @@ class Questioner:
             prompt_version_number=prompt_version_number,
         )
         self.collect_action_clusters = []
-        iterator = action_list
+        iterator = self.all_actions
         if pbar:
-            iterator = tqdm(action_list)
+            iterator = tqdm(self.all_actions)
         for action in iterator:
             user_input = "{\n\t'action':{{ ACTION }}\n\t'cluster_labels':{{ CLUSTER_LABELS }}\n}"
             user_input = user_input.replace("{{ ACTION }}", action)
@@ -167,9 +257,10 @@ class Questioner:
             out_dict.update(out)
             self.collect_action_clusters.append(out_dict)
         self.set_cluster_to_actions_dict()
-        return dict(self.cluster_to_actions_dict)
+        self.cluster_to_actions = self.cluster_to_actions_dict
 
     def set_cluster_to_actions_dict(self) -> None:
+        """Create a dictionary of clusters to actions."""
         collect = defaultdict(list)
         for ac in self.collect_action_clusters:
             action = ac["action"]
@@ -194,25 +285,39 @@ class Questioner:
         collect = dict(sorted(collect.items(), key=lambda item: len(item[1]), reverse=True))
         self.cluster_to_actions_dict = collect
 
+    def set_sorted_actions(self):
+        """Create a list of sorted actions."""
+        sorted_actions = []
+        for value_dict in self.cluster_to_actions.values():
+            for d in value_dict:
+                sorted_actions.append(d["action"])
+        self.sorted_actions = sorted_actions
+
     def set_action_scores(
         self,
         prompt_version_number: int,
-        action_list: List[str],
-        philosophy_list: List[Dict[str, str]],
         force_refresh: bool = False,
         pbar: bool = False,
         verbose: bool = False,
     ):
+        """Create a list of action scores.
+
+        Args:
+            prompt_version_number (int): The version number of the prompt.
+            force_refresh (bool, optional): If true, rerun even if history_key is in history. Defaults to False.
+            pbar (bool, optional): If true, show a progress bar. Defaults to False.
+            verbose (bool, optional): If true, log the prompt and response. Defaults to False.
+        """
         prompt_name = "score_action"
         pc = PromptConstructor(prompt_name=prompt_name, prompt_version_number=prompt_version_number)
         self.collect_action_scores = []
-        iterator = action_list
+        iterator = self.all_actions
         if pbar:
-            iterator = tqdm(action_list)
+            iterator = tqdm(self.all_actions)
         for action in iterator:
             user_input = "{\n\t'action':'{{ ACTION }}'\n\t'philosophies':{{ PHILOSOPHIES }}\n}"
             user_input = user_input.replace("{{ ACTION }}", action)
-            pl = str(philosophy_list)
+            pl = str(self.philosophies)
             pl = pl.replace("[{'", "[\n\t{'")
             pl = pl.replace("'}, {'", "'},\n\t{'")
             pl = pl.replace("'}]", "'}\n\t\t]")
@@ -230,13 +335,10 @@ class Questioner:
                 self.collect_action_scores.append(d_out)
         self.action_scores = self.collect_action_scores
 
-    def create_scorecard(self, action_order: Optional[List[str]] = None) -> None:
+    def create_scorecard(self) -> None:
+        """Create a scorecard of the action scores."""
         # Convert the list of dictionaries into a DataFrame
         df = pd.DataFrame(self.action_scores)
-        if action_order is None:
-            ordered_actions = df["action"].drop_duplicates().to_list()
-        else:
-            ordered_actions = action_order
 
         # Map morality to numerical values
         morality_map = {"moral": 1, "undecided": 0, "immoral": -1}
@@ -248,7 +350,11 @@ class Questioner:
             fill_value=-10,
         )
 
-        df_pivot = df_pivot[ordered_actions]
+        # Sort philosophies by number of 1's, then by number of 0's, then by number of -1's
+        philosophy_reindex = df_pivot.sum(axis=1).sort_values(ascending=False).index
+        df_pivot = df_pivot.reindex(philosophy_reindex)
+
+        df_pivot = df_pivot[self.sorted_actions]
 
         cluster_to_int = {k: e + 1 for e, k in enumerate(self.cluster_to_actions_dict.keys())}
 
@@ -269,7 +375,8 @@ class Questioner:
             aggfunc=lambda x: ", ".join(x),  # Aggregate reasons, if necessary
         ).fillna("")
 
-        hover_data = hover_data[ordered_actions]
+        hover_data = hover_data[self.sorted_actions]
+        hover_data = hover_data.reindex(philosophy_reindex)
 
         philosophy_to_description = {row["name"]: row["description"] for row in self.philosophies}
 
@@ -321,6 +428,33 @@ class Questioner:
 
         # Update layout for better readability
         fig.update_layout(title="Action Scores Heatmap", margin_r=275)
+
+        fig.update_yaxes(autorange="reversed")
+
+        # Colors for the legend (assuming these to represent min, mid, and max of 'Blues_r')
+        colors = [
+            "#f7fbff",  # Lightest blue for 'immoral'
+            "#6baed6",  # Medium blue for 'undecided'
+            "#08306b",
+        ]  # Darkest blue for 'moral'
+
+        # Add dummy traces for the legend items
+        legend_labels = ["immoral", "undecided", "moral"]
+        for color, label in zip(colors, legend_labels):
+            fig.add_trace(
+                go.Scatter(
+                    x=[None],
+                    y=[None],
+                    mode="markers",
+                    marker=dict(size=10, color=color),
+                    legendgroup="group",
+                    showlegend=True,
+                    name=label,
+                )
+            )
+
+        # Adjust layout if necessary
+        fig.update_layout(legend_title_text="Category")
 
         # Save the figure
         # If the path doesn't exist, create it
