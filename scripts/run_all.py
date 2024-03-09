2fbc17a94e3498ee84489fff545a6512ac02945e
JJ <jj.stankowicz@gmail.com>
2024-03-09 00:14:01 -0500

    Refactor argument passing into Questioner.

--- scripts/run_all.py
+++ scripts/run_all.py
@@ -6,43 +6,25 @@ def main():
     ### Get all philosophies ###
     ###
     print("Getting all philosophies...")
-    philosophies = q.get_philosophies(prompt_version_number=1)
+    q.set_philosophies(prompt_version_number=1)
     ### Get all actions from all philosophies ###
     ###
     print("Getting actions from all philosophies...")
-    collect_actions_from_philosophies = []
-    for philosophy_dict in philosophies:
-        actions_from_philosophies = q.get_actions_from_philosophies(
-            prompt_version_number=0,
-            philosophy_dict=philosophy_dict,
-        )
-        collect_actions_from_philosophies.append(
-            {
-                "philo": philosophy_dict,
-                "actions": actions_from_philosophies,
-            }
-        )
+    q.set_all_actions_from_philosophies()
     ### Collect actions into a list ###
     ###
     print("Collecting actions into a list...")
-    collect_all_actions = []
-    for action in collect_actions_from_philosophies:
-        for action_dict in action["actions"]:
-            collect_all_actions.append(action_dict["action"])
+    q.set_all_actions()
     ### Cluster the actions
     ###
     # print("Clustering actions...")
-    clusters_to_actions_dict = q.get_clusters_to_actions(
+    q.set_clusters_to_actions(
         prompt_version_number=0,
-        action_list=collect_all_actions,
         verbose=True,
         pbar=True,
         # force_refresh=True,
     )
-    sorted_actions = []
-    for value_dict in clusters_to_actions_dict.values():
-        for d in value_dict:
-            sorted_actions.append(d["action"])
+    q.set_sorted_actions()
     ### Get the scores for each action
     ###
     print("Getting scores for each action...")
@@ -50,8 +32,6 @@ def main():
     q.set_chatbot(model="gpt-3.5-turbo")
     q.set_action_scores(
         prompt_version_number=0,
-        action_list=sorted_actions,
-        philosophy_list=philosophies,
         verbose=False,
         pbar=True,
         force_refresh=False,
