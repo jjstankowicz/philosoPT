0563ea1d7884cd5524407c55d18e0a00851f61b7
JJ <jj.stankowicz@gmail.com>
2024-03-09 00:13:29 -0500

    Document all utils.

--- philo/utils.py
+++ philo/utils.py
@@ -7,6 +7,11 @@ MessageType = List[Dict[str, str]]
 
 # A method to get parent directory of the current file
 def get_repo_root() -> str:
+    """Get the root directory of the repository
+
+    Returns:
+        str: The absolute path to the root directory of the repository
+    """
     out = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir))
     return out
 
@@ -17,6 +22,14 @@ from ast import literal_eval
 
 # Convert a string of text to a list of dictionaries
 def parse_structured_output(text: str) -> any:
+    """Parse a string of text into python objects
+
+    Args:
+        text (str): The text to parse
+
+    Returns:
+        any: The parsed text as a python object
+    """
     out = text
     # Remove all lines that start with "#" and "`"
     # and remove any comments from the end of a line
@@ -44,6 +57,14 @@ def parse_structured_output(text: str) -> any:
 
 
 def load_history(suffix: str = "") -> dict:
+    """Load the history from a file
+
+    Args:
+        suffix (str, optional): Use for different histories. Defaults to "".
+
+    Returns:
+        dict: The history
+    """
     history_file_path = os.path.join(get_repo_root(), f"history{suffix}.json")
     with open(history_file_path, "r") as f:
         history = json.load(f)
