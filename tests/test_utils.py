615396a0d25f510e38094aa977768421096b7d74
JJ <jj.stankowicz@gmail.com>
2024-03-09 01:09:55 -0500

    Update utils tests.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,8 @@
 import unittest
-from philo.utils import get_prompt, parse_structured_output
+from philo.utils import parse_structured_output, load_history, get_repo_root
 
 
 class TestUtils(unittest.TestCase):
-
-    def test_get_prompt(self):
-        self.prompt = get_prompt("ask_about_philosophies", version_number=0)
-        self.assertIsInstance(self.prompt, str, "Prompt is not a string")
 
     def test_parse_structured_output(self):
         self.text = "[\n {'key': 'This is a test's test'}, \n {'key2': 'This is another's test'}\n]"
@@ -15,3 +11,11 @@ class TestUtils(unittest.TestCase):
         self.assertIsInstance(
             self.list_of_dicts[0], dict, "List of dicts does not contain dictionaries"
         )
+
+    def test_load_history(self):
+        self.history = load_history()
+        self.assertIsInstance(self.history, dict, "History is not a dictionary")
+
+    def test_get_repo_root(self):
+        rr = get_repo_root()
+        self.assertIsInstance(rr, str, "Repo root is not a string")
