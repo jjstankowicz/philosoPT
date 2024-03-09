a674d2da1453a0ceca7513049e0faebf8463b4a8
JJ <jj.stankowicz@gmail.com>
2024-03-09 14:10:16 -0500

    Add load results util.

--- philo/utils.py
+++ philo/utils.py
@@ -1,6 +1,9 @@
 import os
+import re
 import json
-from typing import List, Dict
+from typing import List, Dict, Tuple
+from ast import literal_eval
+import pandas as pd
 
 MessageType = List[Dict[str, str]]
 
@@ -14,10 +17,6 @@ def get_repo_root() -> str:
     """
     out = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir))
     return out
-
-
-import re
-from ast import literal_eval
 
 
 # Convert a string of text to a list of dictionaries
@@ -69,3 +68,15 @@ def load_history(suffix: str = "") -> dict:
     with open(history_file_path, "r") as f:
         history = json.load(f)
     return history
+
+
+def load_results_dfs() -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """Load the results dataframe
+
+    Returns:
+        Tuple[pd.DataFrame, pd.DataFrame]: The results dataframes
+    """
+    results_file_path = os.path.join(get_repo_root(), "results")
+    df_pivot = pd.read_parquet(os.path.join(results_file_path, "df_pivot.parquet"))
+    df_hover_data = pd.read_parquet(os.path.join(results_file_path, "df_hover_data.parquet"))
+    return df_pivot, df_hover_data
