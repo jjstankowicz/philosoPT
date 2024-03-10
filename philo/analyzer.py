8ecac53849de76a7aa3e992cac7d7dfda8bc2534
JJ <jj.stankowicz@gmail.com>
2024-03-10 00:51:26 -0500

    Add dendrogram heatmap functionality

--- philo/analyzer.py
+++ philo/analyzer.py
@@ -1,27 +1,29 @@
 from itertools import product
 import tqdm
+import numpy as np
+import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 from sklearn.decomposition import PCA
+from scipy.spatial import distance_matrix
 from philo.utils import load_results_dfs
 import plotly.graph_objects as go
 import plotly.offline as ol
-import numpy as np
-import pandas as pd
+import plotly.figure_factory as ff
 
 
 class Analyzer:
     def __init__(self):
         self.df, _ = load_results_dfs()
 
-        self.set_action_data()
-        self.set_pca_action_data()
+        self.set_data_action()
+        self.set_data_action_pca()
 
-    def set_action_data(self) -> None:
+    def set_data_action(self) -> None:
         """Set the action_data attribute with the results of the PCA analysis."""
         self.action_data = self.df.values
 
-    def set_pca_action_data(self) -> None:
+    def set_data_action_pca(self) -> None:
         """Set the pca_action_data attribute with the results of the PCA analysis."""
         n_components = 1
         explained_variance = 0.0
@@ -89,9 +91,9 @@ class Analyzer:
         )
         self.df_kmeans = result_df
 
-    def set_kmeans_trace_configs(self) -> None:
+    def set_trace_configs_kmeans(self) -> None:
         """Set the kmeans_trace_configs attribute with the configurations for the KMeans metrics plot."""
-        self.kmeans_trace_configs = {
+        self.trace_configs_kmeans = {
             "Actions Inertia": {
                 "dataframe": self.df_kmeans.xs("actions", level="vector_type"),
                 "y_col": "inertia_mean",
@@ -114,15 +116,15 @@ class Analyzer:
             },
         }
 
-    def set_kmeans_metrics_plot(self) -> None:
+    def set_metrics_plot_kmeans(self) -> None:
         """Set the kmeans_metrics_plot attribute with the Plotly figure for the KMeans metrics plot."""
-        self.set_kmeans_trace_configs()
+        self.set_trace_configs_kmeans()
 
         # Initialize a Plotly figure
         fig = go.Figure()
 
         # Loop over the configuration and create each trace
-        for trace_name, config in self.kmeans_trace_configs.items():
+        for trace_name, config in self.trace_configs_kmeans.items():
             trace = go.Scatter(
                 x=config["dataframe"].index.get_level_values("number_of_clusters"),
                 y=config["dataframe"][config["y_col"]],
@@ -142,8 +144,111 @@ class Analyzer:
             legend_title="Traces",
         )
 
-        self.kmeans_metrics_plot = fig
+        self.metrics_plot_kmeans = fig
 
-    def save_kmeans_metrics_plot(self) -> None:
+    def save_metrics_plot_kmeans(self) -> None:
         """Save the KMeans metrics plot to an HTML file."""
-        ol.plot(self.kmeans_metrics_plot, filename="results/kmeans_metrics.html")
+        ol.plot(self.metrics_plot_kmeans, filename="results/metrics_kmeans.html")
+
+    def set_df_philosophy(self) -> None:
+        """Set the philosophy_df attribute a dataframe of distances."""
+        data = self.action_data
+        # Compute the distance matrix between the philosophy data
+        m_dist = distance_matrix(data, data)
+        # Min-max scale the distance matrix
+        m_dist = (m_dist - m_dist.min()) / (m_dist.max() - m_dist.min())
+        self.df_philosophy = pd.DataFrame(m_dist, index=self.df.index, columns=self.df.index)
+
+    def set_distance_heatmap_philosophy(self):
+        # Extract labels and convert the DataFrame to a numpy array
+        labels = self.df_philosophy.columns.tolist()
+        data_array = self.df_philosophy.values
+
+        # Initialize figure with bottom dendrogram
+        fig = ff.create_dendrogram(data_array, orientation="bottom", labels=labels)
+        for i in range(len(fig["data"])):
+            fig["data"][i]["yaxis"] = "y2"
+
+        # Create Side Dendrogram
+        dendro_side = ff.create_dendrogram(data_array, orientation="right", labels=labels)
+        for i in range(len(dendro_side["data"])):
+            dendro_side["data"][i]["xaxis"] = "x2"
+
+        # Add Side Dendrogram Data to Figure
+        for data in dendro_side["data"]:
+            fig.add_trace(data)
+
+        # Determine the new order based on the dendrogram
+        dendro_leaves_labels = dendro_side["layout"]["yaxis"]["ticktext"]
+        dendro_leaves_indices = [labels.index(i) for i in dendro_leaves_labels]
+
+        # Reorder the data based on the dendrogram
+        data_reordered = data_array[np.ix_(dendro_leaves_indices, dendro_leaves_indices)]
+
+        # Match the ticktext to the tickvals to create a mapping
+        dendro_ticks = dict(
+            zip(
+                dendro_side["layout"]["yaxis"]["ticktext"],
+                dendro_side["layout"]["yaxis"]["tickvals"],
+            )
+        )
+
+        # Map the labels to their corresponding tickvals
+        heatmap_x = [dendro_ticks[x] for x in dendro_leaves_labels]
+        heatmap_y = [dendro_ticks[y] for y in dendro_leaves_labels]
+
+        # Create an outer product of the keys of the dendro_ticks dictionary
+        ordered_philosophy_labels = list(dendro_ticks.keys())
+        hover_text = [
+            [
+                f"Row: {ordered_philosophy_labels[i]}<br>Column: {ordered_philosophy_labels[j]}<br>Value: {data_reordered[i][j]}"
+                for j in range(len(dendro_leaves_labels))
+            ]
+            for i in range(len(dendro_leaves_labels))
+        ]
+
+        heatmap = [
+            go.Heatmap(
+                x=heatmap_x,  # numerical positions from dendrogram tickvals
+                y=heatmap_y,  # numerical positions from dendrogram tickvals
+                z=data_reordered,
+                colorscale="Blues",
+                customdata=np.tile(
+                    dendro_leaves_labels, (len(dendro_leaves_labels), 1)
+                ),  # Repeat the labels for each row
+                hoverinfo="text",
+                hovertext=hover_text,
+            )
+        ]
+
+        # Add Heatmap Data to Figure
+        for data in heatmap:
+            fig.add_trace(data)
+
+        fig.update_layout(
+            # Adjust axis properties
+            xaxis=dict(showgrid=False, ticks="", side="bottom", domain=[0.15, 1]),
+            yaxis=dict(showgrid=False, showticklabels=False, ticks="", domain=[0, 0.85]),
+            xaxis2=dict(showgrid=False, showticklabels=False, domain=[0, 0.15]),
+            yaxis2=dict(showgrid=False, showticklabels=False, domain=[0.825, 0.975]),
+            # Set the background color for the plot
+            plot_bgcolor="white",
+            # Set dimensions
+            width=800,
+            height=800,
+            # Don't show the legend
+            showlegend=False,
+            # Set hovermode to closest
+            hovermode="closest",
+            # Add title
+            title="Distance Heatmap of Philosophy Data<br>0 is close<br>1 is far",
+        )
+
+        # Assign the figure to a class attribute
+        self.distance_heatmap_philosophy = fig
+
+    def save_distance_heatmap_philosophy(self) -> None:
+        """Save the distance heatmap to an HTML file."""
+        ol.plot(
+            self.distance_heatmap_philosophy, filename="results/distance_heatmap_philosophy.html"
+        )
