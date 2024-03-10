655c73c986a4603246d4b53abf02fb3f2d8e3575
JJ <jj.stankowicz@gmail.com>
2024-03-10 00:50:21 -0500

    Rename file.

--- scripts/create_metrics_plots_kmeans.py
+++ scripts/create_metrics_plots_kmeans.py
@@ -4,8 +4,8 @@ from philo.analyzer import Analyzer
 def main():
     analyzer = Analyzer()
     analyzer.set_df_kmeans()
-    analyzer.set_kmeans_metrics_plot()
-    analyzer.save_kmeans_metrics_plot()
+    analyzer.set_metrics_plot_kmeans()
+    analyzer.save_metrics_plot_kmeans()
 
 
 if __name__ == "__main__":
